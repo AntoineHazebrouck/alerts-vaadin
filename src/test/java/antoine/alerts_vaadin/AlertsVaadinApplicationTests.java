3df76d9050ea0fefdce76c3bcaa9b44f00783236
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-09-03 12:38:53 +0200

    fixed test

--- src/test/java/antoine/alerts_vaadin/AlertsVaadinApplicationTests.java
+++ src/test/java/antoine/alerts_vaadin/AlertsVaadinApplicationTests.java
@@ -2,12 +2,12 @@ package antoine.alerts_vaadin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class AlertsVaadinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
