b9f5f35263ecb508a62a3b9d3ceeada960a9e93d
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-09-05 11:20:55 +0200

    fix

--- src/main/java/antoine/alerts_vaadin/services/command/DeleteAlert.java
+++ src/main/java/antoine/alerts_vaadin/services/command/DeleteAlert.java
@@ -2,6 +2,7 @@ package antoine.alerts_vaadin.services.command;
 
 import antoine.alerts_vaadin.entities.Alert;
 import antoine.alerts_vaadin.repositories.AlertsRepository;
+import antoine.alerts_vaadin.services.AlertsScheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Service;
 public class DeleteAlert implements Command<Alert, Void> {
 
     private final AlertsRepository alerts;
+    private final AlertsScheduler scheduler;
 
     @Override
     public Void execute(Alert alert) {
         alerts.delete(alert);
+        scheduler.cancel(alert);
         return null;
     }
 }
